d60bc8d86ae1c41c3e29e1f12f4737333a622157
bassamchahine <65299482+bassamchahine@users.noreply.github.com>
2024-06-12 16:03:46 -0500

    diag6

--- pg_database/tf.sh
+++ pg_database/tf.sh
@@ -1,8 +1,8 @@
 terraform plan -detailed-exitcode -no-color -out tfplan-var \
 -var "azure_subscription_id = ${AZURE_SUBSCRIPTION_ID}" \
--var "azure_tenant_id = ${{AZURE_TENANT_ID}}" \
--var "azure_client_id = ${{ secrets.azure_client_id}}" \
--var "azure_client_secret = ${{ secrets.azure_client_secret}}" \
+-var "azure_tenant_id = ${AZURE_SUBSCRIPTION_ID}" \
+-var "azure_client_id = ${AZURE_CLIENT_ID}" \
+-var "azure_client_secret = ${secrets.azure_client_secret}" \
 -var "instaclustr_api_cred = ${{ secrets.instaclustr_api_cred}}" \
 -var "rg_name = ${{ var.rg_name}}" \
 -var "rg_location = ${{ var.rg_location}}" \
