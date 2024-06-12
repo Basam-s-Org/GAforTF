aae1b17f35435fa1608f73af36c679b8722e8d10
bassamchahine <65299482+bassamchahine@users.noreply.github.com>
2024-06-12 15:48:06 -0500

    added tf.sh

---
+++ pg_database/tf.sh
@@ -0,0 +1,15 @@
+terraform plan -detailed-exitcode -no-color -out tfplan-var \
+-var "azure_subscription_id = ${{ secrets.azure_subscription_id}}" \
+-var "azure_tenant_id = ${{ secrets.azure_tenant_id}}" \
+-var "azure_client_id = ${{ secrets.azure_client_id}}" \
+-var "azure_client_secret = ${{ secrets.azure_client_secret}}" \
+-var "instaclustr_api_cred = ${{ secrets.instaclustr_api_cred}}" \
+-var "rg_name = ${{ var.rg_name}}" \
+-var "rg_location = ${{ var.rg_location}}" \
+-var "db_name = ${{ var.db_name}}" \
+-var "db_version = ${{ var.db_version}}" \
+-var "db_secret_kv = ${{ var.db_secret_kv}}" \
+-var "firewall_rules = ${{ var.firewall_rules}}" \
+-var "cloud_provider = ${{ var.cloud_provider}}" \
+-var "db_extensions = ${{ var.db_extensions}}" \
+-var "dc_name = ${{ var.dc_name}}" || export exitcode=$?
