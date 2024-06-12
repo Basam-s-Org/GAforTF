1a9ce822a7dc1ac10f01aca9d9f86a96d89cb7d7
bassamchahine <65299482+bassamchahine@users.noreply.github.com>
2024-06-12 18:30:30 -0500

    diag35

--- pg_database/build_tfvars.sh
+++ pg_database/build_tfvars.sh
@@ -1,5 +1,5 @@
 echo -e "azure_subscription_id=\""${TF_VAR_AZURE_SUBSCRIPTION_ID}"\"\n" > ./terraform.tfvars
-echo -e "azure_tenant_i=\""${TF_VAR_TENANT_ID}"\"\n" >> ./terraform.tfvars
+echo -e "azure_tenant_id=\""${TF_VAR_TENANT_ID}"\"\n" >> ./terraform.tfvars
 echo -e "azure_client_id=\""${TF_VAR_AZURE_CLIENT_ID}"\"\n">> ./terraform.tfvars
 echo -e "azure_client_secret=\""${TF_VAR_CLIENT_SECRET}"\"\n" >> ./terraform.tfvars
 echo -e "instaclustr_api_cred=\""${TF_VAR_INSTACLUSTR_API_CRED}"\"\n" >> ./terraform.tfvars
@@ -8,9 +8,9 @@ echo -e "rg_location=\""${TF_VAR_RG_LOCATION}"\"\n" >> ./terraform.tfvars
 echo -e "db_name=\""${TF_VAR_DB_NAME}"\"\n" >> ./terraform.tfvars
 echo -e "db_version=\""${TF_VAR_DB_VERSION}"\"\n">> ./terraform.tfvars
 echo -e "db_secret_kv=\""${TF_VAR_DB_SECRET_KV}"\"\n">> ./terraform.tfvars
-echo -e "firewall_rules=\""${TF_VAR_FIREWALL_RULES}"\"\n">> ./terraform.tfvars
+echo -e "firewall_rules="${TF_VAR_FIREWALL_RULES}"\n">> ./terraform.tfvars
 echo -e "cloud_provider=\""${TF_VAR_CLOUD_PROVIDER}"\"\n" >> ./terraform.tfvars
-echo -e "db_extensions=\""${TF_VAR_DB_EXTENSIONS}"\"\n" >> ./terraform.tfvars
+echo -e "db_extensions="${TF_VAR_DB_EXTENSIONS}"\n" >> ./terraform.tfvars
 echo -e "dc_name=\""${TF_VAR_DC_HOME}"\"\n" >> ./terraform.tfvars
 cat ./terraform.tfvars
 
