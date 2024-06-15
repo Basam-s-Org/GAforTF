bbc15b2d0d7e70003e3ff783c3398988007a3177
bassamchahine <65299482+bassamchahine@users.noreply.github.com>
2024-06-14 21:18:05 -0500

    add destroy

--- pg_database/build_tfvars.sh
+++ pg_database/build_tfvars.sh
@@ -1,5 +1,3 @@
-terraform import azurerm_resource_group.org_rg /subscriptions/7a07f268-eb64-45df-b63e-b5595e713287/resourceGroups/bassam-org-database
-
 echo -e "azure_subscription_id=\""${TF_VAR_AZURE_SUBSCRIPTION_ID}"\"\n" > ./terraform.tfvars
 echo -e "azure_tenant_id=\""${TF_VAR_AZURE_TENANT_ID}"\"\n" >> ./terraform.tfvars
 echo -e "azure_client_id=\""${TF_VAR_AZURE_CLIENT_ID}"\"\n">> ./terraform.tfvars
